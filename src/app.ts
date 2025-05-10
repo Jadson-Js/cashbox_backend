fccf4634a06a6d8846a38955349e5f531e5652f4
jadson-js <jadson20051965@gmail.com>
2025-05-10 12:41:05 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import userRoutes from './modules/user/routes/user.route';
 import categoryRoutes from './modules/category/routes/category.route';
 import transactionRoutes from './modules/transaction/routes/transaction.route';
 import { errorHandler } from './shared/middlewares/errorHandler';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/users', userRoutes);
